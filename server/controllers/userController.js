b975abfc4acb723f6ba7063a3439446f57247e52
zunayed-arp <zunayed.arp@gmail.com>
2021-12-10 15:03:20 +0600

    refactgoring and crud complete

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,38 +1,38 @@
-import express from "express";
-import models from "../models";
-import { saveUser } from "../services/userService"
+import express, { response } from "express";
+import { saveUser, getAllUsers, update, deleteById } from "../services/userService"
 
 const router = express.Router();
 
-
-
 const getHandler = async (req, res) => {
 	const users = await getAllUsers()
-	res.send(users);
-
-
-
-
-
-	// const params = JSON.stringify(req.query.id)
-	// console.log('Parcel koi!!!', params);
-	// res.send('Hitting the post ' + params)
+	res.status(200).send(users);
 };
-
 
 const postHandler = async (req, res) => {
 	const body = req.body;
-
 	const user = await saveUser(body);
+	res.status(201).send(user._id)
+};
 
-	res.send(user._id)
-
-
+const putHandler = async (req, res) => {
+	const body = req.body;
+	const user = await update(body);
+	res.status(200).send(user._id)
 };
 
 
-router.get('/', getHandler)
-router.post('/', postHandler)
+const deleteHandler = async (req, res) => {
+	const id = req.params.id;
+	// console.log(req.params.id)
+	await deleteById(id);
+	res.status(200).send("User deleted");
+}
+
+
+router.get('/', getHandler);
+router.post('/', postHandler);
+router.put('/', putHandler);
+router.delete('/', deleteHandler);
 
 const configure = (app) => {
 	app.use('/users', router);
