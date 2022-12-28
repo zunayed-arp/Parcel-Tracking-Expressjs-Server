d288d4f9430dfb8e3cc4b0b38009a14eb21b23a2
zunayed-arp <zunayed.arp@gmail.com>
2022-12-28 21:27:25 +0600

    fixed validation bug

--- middlewares/handleValidations.js
+++ middlewares/handleValidations.js
@@ -3,7 +3,7 @@ import { BadRequest } from "../utils/errors";
 export const handleValidation = (validate) => {
   return (req, res, next) => {
     const result = validate(req.body);
-    const isValid = result.error === null;
+    const isValid = result.error == null;
     if (isValid) {
       return next();
     }
