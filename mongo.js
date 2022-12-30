57654b7d7d11b2a24606ef7d30c277b6667b4022
zunayed-arp <zunayed.arp@gmail.com>
2022-12-30 21:41:19 +0600

    configured and refactored

--- mongo.js
+++ mongo.js
@@ -1,18 +1,14 @@
 import mongoose from "mongoose";
 
-
 export const uri = "mongodb://localhost:27017/parcelkoi";
 const options = {};
-
-
 
 const log = (msg) => console.log(msg);
 
 export const connectWithDb = () => {
   mongoose.connect(uri, options, (err, db) => {
     if (err) {
-      console.log("error");
-    } else log("database connection established");
+      throw err;
+    }
   });
 };
-
