4315cc66864b717f959e7364b560ec9b895cab15
zunayed-arp <zunayed.arp@gmail.com>
2023-01-01 01:45:26 +0600

    write first test case with spuertest

--- app.test.js
+++ app.test.js
@@ -1,5 +1,31 @@
-import app from './app';
+import request from "supertest";
+import app from "./app";
 
-test('should work',async ()=>{
-    console.log('my first test')
+beforeAll(async ()=>{
+    console.log('before all');
+});
+
+afterAll(async ()=>{
+    console.log('after all');
+});
+
+
+beforeEach(async ()=>{
+    console.log("before each")
+});
+
+afterEach(async ()=>{
+    console.log("After Each")
+});
+
+describe("user controller test suite", () => {
+  test("should work", async () => {
+    console.log("my first test");
+  });
+  test("get all users should return list of users", async () => {
+    console.log("get all users test");
+    let response = await request(app).get('/users');
+    expect(response).not.toBeNull();
+    
+  });
 });
