a090d1163355f4f0a796b776ce861e0ef7e4adf5
zunayed-arp <zunayed.arp@gmail.com>
2023-01-30 16:31:38 +0600

    adding id to test case

--- src/services/__mocks__/userService.js
+++ src/services/__mocks__/userService.js
@@ -1,9 +1,21 @@
-export const getAllUsers = async () => {
-    const users = [
+import models from "../../models";
+
+let users = [
         {
-            '_id':'1','username':'test001'
+          _id: "1",
+          username: "test001",
         }
-    ];
-  
-    return users;
-  };
+];
+
+
+export const getAllUsers = async () => {
+
+  return users;
+};
+
+export const saveUser = async (user) => {
+  const model = new models.User(user);
+  model._id = '2';
+  users.push(model);
+  return model;
+};
--- tests/userController.test.js
+++ tests/userController.test.js
@@ -5,11 +5,23 @@ import app from "../src/app";
 jest.mock('../src/services/userService');
 
 describe('userController Test Suite', ()=>{
-    test('getAllusers should return an array of users.',async ()=>{
+    test('get should return an array of users.',async ()=>{
         let response = await request(app).get('/users');
         expect(response.statusCode).toBe(200);
         let users = response.body;
         expect(users.length).toBeGreaterThan(0);
         expect(users[0]._id).toBe('1');
     });
+    test.only('post should return an array of users.',async ()=>{
+        let user = {username: 'test002'};
+        let response = await request(app).post('/users').send(user);
+        console.log('response test only',response)
+        expect(response.statusCode).toBe(201);
+        let body = response.body;
+        expect(body.length).toBe('24');
+        // 63d79bf6f8f543f51bba5655
+        
+    });
+
+
 })
