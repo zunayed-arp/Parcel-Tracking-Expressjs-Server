08b9abb7ece594ffd8787fd01d0196a845f25746
zunayed-arp <zunayed.arp@gmail.com>
2023-02-02 15:04:02 +0600

    remove const from variable code.fixed this bug after 2/3 hr

--- src/middlewares/handleErrors.js
+++ src/middlewares/handleErrors.js
@@ -3,7 +3,7 @@ import { GeneralError } from "../utils/errors";
 export const handleErrors = async (err, req, res, next) => {
   let code = 500;
   if (err instanceof GeneralError) {
-    const code = err.getCode();
+    code = err.getCode();
   }
 
   let correlationId = req.headers["x-correlation-id"];
